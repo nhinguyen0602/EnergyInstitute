0015926207353fa8b7fbd38104945c8c930c1c3d
Nhi Nguyen <nhinguyen@novahub.vn>
2020-03-11 14:34:26 +0700

    create model sector

--- app/models/subsector.rb
+++ app/models/subsector.rb
@@ -1,3 +1,4 @@
 class Subsector < ApplicationRecord
   validates :name, presence: true
+  has_many :sectors, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_094530) do
+ActiveRecord::Schema.define(version: 2020_03_11_072539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "sectors", force: :cascade do |t|
+    t.bigint "subsector_id", null: false
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["subsector_id"], name: "index_sectors_on_subsector_id"
+  end
+
+  create_table "subsectors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -24,4 +38,5 @@ ActiveRecord::Schema.define(version: 2020_03_09_094530) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "sectors", "subsectors"
 end
