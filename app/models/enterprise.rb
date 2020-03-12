7c13d8c6b3e77240c162884563e27bc5dd355fb3
Nhi Nguyen <nhinguyen@novahub.vn>
2020-03-12 10:26:56 +0700

    fomat code

--- app/models/enterprise.rb
+++ app/models/enterprise.rb
@@ -1,8 +1,8 @@
 class Enterprise < ApplicationRecord
-    validates :name, presence: true
-    belongs_to :sector
-    has_many :users
-    has_one :enterprise_detail
-    has_many :energy_consumptions
-    has_many :products
+  validates :name, presence: true
+  belongs_to :sector
+  has_many :users
+  has_one :enterprise_detail
+  has_many :energy_consumptions
+  has_many :products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-    belongs_to :enterprise
-    has_many :productivities
-    validates :name, presence: true
+  belongs_to :enterprise
+  has_many :productivities
+  validates :name, presence: true
 end
