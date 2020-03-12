f4aaace596995f417e24f3d44b54c3665014da45
Nhi Nguyen <nhinguyen@novahub.vn>
2020-03-12 09:38:32 +0700

    change validate in model EnterpriseDetail

--- app/models/enterprise_detail.rb
+++ app/models/enterprise_detail.rb
@@ -2,5 +2,8 @@ class EnterpriseDetail < ApplicationRecord
   validates :provincial, presence: true
   validates :district, presence: true
   validates :commune, presence: true
+  validates :year_of_investigation, presence: true
+  validates :number_of_employee, presence: true
+  validates :production_value, presence: true
   belongs_to :enterprise
 end
