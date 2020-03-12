482e4ddaf1a50aa2bfd338ddd660816b169ae4fa
Nhi Nguyen <nhinguyen@novahub.vn>
2020-03-12 15:15:59 +0700

    format code

--- app/models/greenhouse_emission_detail.rb
+++ app/models/greenhouse_emission_detail.rb
@@ -1,4 +1,4 @@
 class GreenhouseEmissionDetail < ApplicationRecord
-    belongs_to :emission_reason
-    belongs_to :greenhouse_emission
+  belongs_to :emission_reason
+  belongs_to :greenhouse_emission
 end
