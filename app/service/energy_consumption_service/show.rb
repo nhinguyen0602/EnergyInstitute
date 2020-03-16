6cf3a8731220e5f6f7d80c96b7ce16846c4d4bb8
Nhi Nguyen <nhinguyen@novahub.vn>
2020-03-16 11:40:25 +0700

    add show service of EnergyConsumption

--- app/service/energy_consumption_service/show.rb
+++ app/service/energy_consumption_service/show.rb
@@ -1,5 +1,9 @@
 module EnergyConsumptionService::Show
   def self.call(enterprise_id, year)
-    
+    if year ==''
+      EnergyConsumption.belong_enterprise(enterprise_id)
+    else
+      EnergyConsumption.belong_enterprise(enterprise_id).at_year(year)
+    end
   end  
 end
