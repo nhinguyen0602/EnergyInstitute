06d66d2ed43fbde389e0fd7ef11551272c4fafe2
Nhi Nguyen <nhinguyen@novahub.vn>
2020-03-16 13:49:05 +0700

    config routes

--- app/controllers/energy_consumptions_controller.rb
+++ app/controllers/energy_consumptions_controller.rb
@@ -1,2 +1,9 @@
 class EnergyConsumptionsController < ApplicationController
+  def show
+    energy_consumptions = EnergyConsumptionService::Show.call(@current_user.enterprise.id, params[:year])  
+    data = {
+      energy_consumptions: energy_consumptions
+    }
+    render_data data
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
     scope :enterprises do
       get '', to: 'enterprises#show'
       put '', to: 'enterprises#update'
+      scope :energy_connsumptions do
+        get '', to: 'energy_consumptions#show'
+      end
     end
   end
 end
