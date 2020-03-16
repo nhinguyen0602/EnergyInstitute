8ba6b1a4c3dedd6325a5ceadacb9b3757b422ed3
KunlezIsme <khanhnguyen@novahub.vn>
2020-03-16 09:06:48 +0700

    init

--- Gemfile
+++ Gemfile
@@ -7,9 +7,9 @@ ruby '2.6.3'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.1'
 
 gem 'active_model_serializers', '~> 0.10.0'
-
+gem 'pg', '~> 1.2', '>= 1.2.2'
 # Use postgresql as the database for Active Record
-gem 'pg', '>= 0.18', '< 2.0'
+# gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
@@ -44,3 +44,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'rack-cors'
--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ Bundler.require(*Rails.groups)
 
 module EnergyInstitute
   class Application < Rails::Application
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
@@ -33,5 +34,12 @@ module EnergyInstitute
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: :any
+      end
+    end
   end
 end
