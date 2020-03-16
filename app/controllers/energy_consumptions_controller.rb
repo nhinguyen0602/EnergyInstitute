1ef8516bec7abe2657aca1dc76da03df3de01b7c
Nhi Nguyen <nhinguyen@novahub.vn>
2020-03-16 14:23:06 +0700

    add energy_consumption_params to energyconsumption controller

--- app/controllers/energy_consumptions_controller.rb
+++ app/controllers/energy_consumptions_controller.rb
@@ -6,4 +6,19 @@ class EnergyConsumptionsController < ApplicationController
     }
     render_data data
   end
+
+  def update
+    energy_consumptions = EnergyConsumptionService::Update.call(@current_user.enterprise.id, energy_consumption_params)  
+    data = {
+      energy_consumptions: energy_consumptions
+    }
+    render_data data
+  end
+
+  def energy_consumption_params
+    params.require(%i[year_of_investigation self_produced_electricity consumption_electricity biomass_energy renewable_energy])
+    params.permit(:year_of_investigation, :self_produced_electricity, :consumption_electricity, :biomass_energy, :renewable_energy
+                , :coal, :bitum_coal, :coke_coal, :dust_coal, :ko, :do, :fo, :lpg, :ng)
+  end
+  
 end
