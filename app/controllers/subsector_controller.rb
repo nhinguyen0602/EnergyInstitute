92b7cb118ac0433b5aee747c4227a03d62686ed5
Nhi Nguyen <nhinguyen@novahub.vn>
2020-03-17 14:11:19 +0700

    done api show subsector

--- app/controllers/subsector_controller.rb
+++ app/controllers/subsector_controller.rb
@@ -1,9 +1,19 @@
 class SubsectorController < ApplicationController
+  
   def index
     subsectors = SubsectorService::Index.call
     data = {
       subsectors: subsectors
     }
     render_data data
-  end  
+  end
+  
+  def show
+    subsector = SubsectorService::Show.call
+    data = {
+      subsector: subsector
+    }
+    render_data data
+  end
+
 end
