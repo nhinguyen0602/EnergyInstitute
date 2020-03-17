8786664d49059289f71034140806e5cd08cee762
Nhi Nguyen <nhinguyen@novahub.vn>
2020-03-17 11:28:13 +0700

    add scope order by year in enterprisedetail

--- app/service/enterprise_service/show.rb
+++ app/service/enterprise_service/show.rb
@@ -1,6 +1,6 @@
 module EnterpriseService::Show
   def self.call(user, year)
-    enterprise_details = EnterpriseDetail.belong_enterprise(user.enterprise)
+    enterprise_details = EnterpriseDetail.belong_enterprise(user.enterprise).order_by_year
     enterprise_details =  enterprise_details.at_year(year) if year.present?
     return user.enterprise, enterprise_details
   end  
