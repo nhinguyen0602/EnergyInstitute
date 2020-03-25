466049ca01fa1cb06ce6e555a5c1ea89c96ddee7
Nhi Nguyen <nhinguyen@novahub.vn>
2020-03-25 09:52:23 +0700

    order energy consumption by year

--- app/service/energy_consumption_service/show.rb
+++ app/service/energy_consumption_service/show.rb
@@ -1,6 +1,6 @@
 module EnergyConsumptionService::Show
   def self.call(enterprise_id, year)
-      energy_consumptions = EnergyConsumption.belong_enterprise(enterprise_id)
+      energy_consumptions = EnergyConsumption.belong_enterprise(enterprise_id).order_by_year
       energy_consumptions = energy_consumptions.at_year(year) if year.present?
       energy_consumptions
   end  
