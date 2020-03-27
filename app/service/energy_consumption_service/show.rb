1c4ea10294146a32b1089bdb7191e3166d36a9e5
Nhi Nguyen <nhinguyen@novahub.vn>
2020-03-27 10:53:30 +0700

    format code

--- app/service/energy_consumption_service/show.rb
+++ app/service/energy_consumption_service/show.rb
@@ -1,7 +1,5 @@
 module EnergyConsumptionService::Show
   def self.call(enterprise, year)
-      energy_consumptions = EnergyConsumption.belong_enterprise(enterprise.id).order_by_year
-      energy_consumptions = energy_consumptions.at_year(year) if year.present?
-      energy_consumptions
+    EnergyConsumption.belong_enterprise(enterprise.id).at_year(year).order_by_year
   end  
 end
--- app/service/energy_consumption_service/update.rb
+++ app/service/energy_consumption_service/update.rb
@@ -1,11 +1,11 @@
 module EnergyConsumptionService::Update
   def self.call(enterprise, energy_consumption_params)
-    energy_consumption_at_year = EnergyConsumption.belong_enterprise(enterprise.id).at_year(energy_consumption_params[:year_of_investigation]).first
-    if energy_consumption_at_year
-      energy_consumption_at_year.update!(energy_consumption_params)
-    else
-      energy_consumption_at_year = Enterprise.find(enterprise.id).energy_consumptions.create!(energy_consumption_params)
-    end
+    energy_consumption_at_year = EnergyConsumption.find_or_initialize_by(
+      enterprise_id: enterprise.id,
+      year_of_investigation: energy_consumption_params[:year_of_investigation]
+    )
+    energy_consumption_at_year.assign_attributes(energy_consumption_params.except(:year_of_investigation))
+    energy_consumption_at_year.save!
     energy_consumption_at_year
   end
 end
