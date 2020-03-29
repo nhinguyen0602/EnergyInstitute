99a6c941c74b929ff3cac56ddcdcbf1cc202f163
Nhi Nguyen <nhinguyen@novahub.vn>
2020-03-29 15:57:45 +0700

    sort greenhouse emission by year

--- app/models/greenhouse_emission.rb
+++ app/models/greenhouse_emission.rb
@@ -3,4 +3,5 @@ class GreenhouseEmission < ApplicationRecord
   belongs_to :product, optional: true
   has_many :greenhouse_emission_details, dependent: :destroy
   validates :year_of_investigation, presence: true
+  scope :order_by_year, ->{order("year_of_investigation DESC")}
 end
