8f870e400410253046874df2aeb94dcd3c4a40c2
Nhi Nguyen <nhinguyen@novahub.vn>
2020-03-30 17:52:59 +0700

    format code

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,9 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  scope :order_by_year, ->{order("year_of_investigation DESC")}
-  scope :at_year, ->(year){
+  scope :order_by_year, -> { order("year_of_investigation DESC") }
+  scope :at_year, ->(year) {
     if year
-      where("year_of_investigation =?", year)
+      where("year_of_investigation = ?", year)
     else
       all
     end
