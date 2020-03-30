0e0a30369841cf6d656cf50edc409a927d1bdf26
Nhi Nguyen <nhinguyen@novahub.vn>
2020-03-30 14:29:53 +0700

    change respone render_data

--- app/service/greenhouse_emission_service/update_or_create.rb
+++ app/service/greenhouse_emission_service/update_or_create.rb
@@ -8,7 +8,7 @@ module GreenhouseEmissionService::UpdateOrCreate
                                 .greenhouse_emission_details
                                 .find_or_create_by!(
                                  emission_reason_id: greenhouse_emission_params[:emission_reason_id]
-                                ) if EmissionReason.find(greenhouse_emission_params[:emission_reason_id])
+                                ) 
     greenhouse_emission_detail.update!(greenhouse_emission_params.except(:year_of_investigation))
     greenhouse_emission
   end
