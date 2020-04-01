0287905ffe322bad75ab2a1e5d089043d1df9bed
Nhi Nguyen <nhinguyen@novahub.vn>
2020-04-01 18:57:54 +0700

    product

--- app/service/data_model/energy_consumption.rb
+++ app/service/data_model/energy_consumption.rb
@@ -1,19 +1,19 @@
 module DataModel::EnergyConsumption
   def self.call
     xlsx = Roo::Spreadsheet.open(Rails.root.join('app/data/data.xlsx').to_s)
-    sheet = xlsx.sheet('TDNL')
+    sheet = xlsx.sheet('SPSX')
     (7..sheet.last_row).map do |index|
       row = sheet.row(index)
       {
         nam: row[0],
         ma_so_doanh_nghiep:row[2],
         ten_doanh_nghiep: row[3],
-        ma_cap: row[4],
-        ten_nganh: row[5],
-        dien_nltt: row[6],
-        bitum_nltt: row[7],
-        than_coc_nltt: row[8],
-        ko_nltt: row[9],
+        stt: row[4],
+        ten_san_pham: row[5],
+        ma_cap_2: row[6],
+        ten_nganh_cap_2: row[7],
+        don_vi: row[8],
+        so_luong: row[9],
         do_nltt: row[10],
         fo_nltt: row[11],
         lpg_nltt: row[12],
