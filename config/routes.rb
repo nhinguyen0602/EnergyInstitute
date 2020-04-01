f5c2ae18d33fcd19ed7a4b05adff8f0d98b15e52
Nhi Nguyen <nhinguyen@novahub.vn>
2020-04-01 16:48:07 +0700

    get data of enterprise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   scope :api do
+    scope :datas do
+      get 'enterprises', to: 'datas#enterprise'
+    end
     scope :auth do 
       post 'sign_in', to: 'auth#sign_in'
       post 'sign_up', to: 'auth#sign_up'
