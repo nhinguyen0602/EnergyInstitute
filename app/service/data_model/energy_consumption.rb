6e65a53d030e7afe63b1034dd7abfa23bc854b9e
Nhi Nguyen <nhinguyen@novahub.vn>
2020-04-01 20:53:17 +0700

    fix product

--- app/service/data_model/energy_consumption.rb
+++ app/service/data_model/energy_consumption.rb
@@ -1,38 +1,46 @@
-module DataModel::EnergyConsumption
-  def self.call
-    xlsx = Roo::Spreadsheet.open(Rails.root.join('app/data/data.xlsx').to_s)
-    sheet = xlsx.sheet('SPSX')
-    (7..sheet.last_row).map do |index|
-      row = sheet.row(index)
-      {
-        nam: row[0],
-        ma_so_doanh_nghiep:row[1],
-        ten_doanh_nghiep: row[2],
-        stt: row[3],
-        ten_san_pham: row[4],
-        ma_cap_2: row[5],
-        ten_nganh_cap_2: row[6],
-        don_vi: row[7],
-        so_luong: row[8],
-        do_nltt: row[9],
-        fo_nltt: row[10],
-        lpg_nltt: row[11],
-        ng_nltt: row[12],
-        npk_pnl: row[13],
-        hs_pnl: row[14],
-        than_pnl: row[15],
-        ng_pnl: row[16],
-        dien_pnl: row[17],
-        antracite_nltt_tj: row[18],
-        bitum_nltt_tj: row[19],
-        than_coc_nltt_tj: row[20],
-        ko_nltt_tj: row[21],
-        do_nltt_tj: row[22],
-        fo_nltt_tj: row[23],
-        lpg_nltt_tj: row[24],
-        ng_nltt_tj: row[25],
-        tong: row[26]
-      }
-    end
-  end
-end
+# module DataModel::EnergyConsumption
+#   def self.call
+#     xlsx = Roo::Spreadsheet.open(Rails.root.join('app/data/data.xlsx').to_s)
+#     sheet = xlsx.sheet('SPSX')
+#     (7..sheet.last_row).map do |index|
+#       row = sheet.row(index)
+#       {
+#         nam: row[0],
+#         ma_so_doanh_nghiep:row[1],
+#         ten_doanh_nghiep: row[2],
+#         ma_cap_2: row[3],
+#         ten_nganh_cap_2: row[4],
+#         he_so_su_dung_nang_luong: row[5],
+#         dien: row[6],
+#         antracite_co2: row[7],
+#         antracite_ch4: row[8],
+#         antracite_n2o: row[9],
+#         coke_co2: row[10],
+#         coke_ch4: row[11],
+#         coke_n2o: row[12],
+#         bitum_co2: row[13,
+#         bitum_ch4: row[14],
+#         bitum_n2o: row[15],
+#         do_co2: row[16],
+#         do_ch4: row[17],
+#         do_n2o: row[18],
+#         fo_co2: row[19],
+#         fo_ch4: row[20],
+#         fo_n2o: row[21],
+#         lpg_co2: row[22],
+#         lpg_ch4: row[23],
+#         lpg_n2o: row[24],
+#         khi_tu_nhien_co2: row[25],
+#         khi_tu_nhien_ch4: row[26],
+#         khi_tu_nhien_n2o: row[27],
+#         nang_luong_sinh_khoi_co2: row[28],
+#         nang_luong_sinh_khoi_ch4: row[29],
+#         nang_luong_sinh_khoi_n2o: row[30],
+#         tong_co2: row[31],
+#         tong_ch4: row[32],
+#         tong_n2o: row[33],
+#         tong: row[34]
+#       }
+#     end
+#   end
+# end
