2638815b24045546bc15c8d6570c3c5673419d0e
Nhi Nguyen <nhinguyen@novahub.vn>
2020-04-01 21:15:00 +0700

    get greenhouse emission product

--- app/service/data_model/enterprise.rb
+++ app/service/data_model/enterprise.rb
@@ -1,23 +1,23 @@
 module DataModel::Enterprise
   def self.call
     xlsx = Roo::Spreadsheet.open(Rails.root.join('app/data/data.xlsx').to_s)
-    sheet = xlsx.sheet('TTDN')
+    sheet = xlsx.sheet('KNK_SPSX')
     (5..sheet.last_row).map do |index|
       row = sheet.row(index)
       {
         nam: row[0],
-        ma: row[1],
-        ten:row[2],
-        tinh: row[3],
-        huyen: row[4],
-        xa: row[5],
-        toa_do_x: row[6],
-        toa_do_y: row[7],
-        nganh_cap_1: row[8],
-        ma_cap_2: row[9],
-        ten_nganh_cap_2: row[10],
-        gtsx: row[11],
-        so_lao_dong: row[12]
+        ma_so_doanh_nghiep: row[1],
+        ten_doanh_nghiep:row[2],
+        ma_nganh_2: row[3],
+        te_cap_2: row[4],
+        ma_sp: row[5],
+        ten_sp: row[6],
+        don_vi: row[7],
+        khoi_luong: row[8],
+        hspt_co2: row[9],
+        hspt_ch4: row[10],
+        phat_thai_co2: row[11],
+        phat_thai_ch4: row[12]
       }
     end
   end
