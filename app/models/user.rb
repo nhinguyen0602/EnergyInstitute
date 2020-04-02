fc800ed5587d7a4cde7bc8240965906331b136b5
Nhi Nguyen <nhinguyen@novahub.vn>
2020-04-02 12:37:17 +0700

    valid email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name, presence: true
-  validates :email, presence: true, length: { maximum: 255 },
-                  format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, length: { maximum: 255 }
   has_secure_password
   validates :password, :on => :create ,
   confirmation: { case_sensitive: true },
