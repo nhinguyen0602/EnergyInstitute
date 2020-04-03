3d565c2eb186319fbe627e29dc5e8f668bfc9cd7
Nhi Nguyen <nhinguyen@novahub.vn>
2020-04-03 13:50:11 +0700

    fix model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   validates :password, :on => :create ,
   confirmation: { case_sensitive: true },
   length: { minimum: 8, maximum: 32 }
-  belongs_to :role
+  belongs_to :role, optional: true
 end
