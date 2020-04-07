a6046aef26a027894429bdc68495f916ddec5c26
Nhi Nguyen <nhinguyen@novahub.vn>
2020-04-07 08:39:31 +0700

    get all enterprise

--- app/service/demo_service/enterprise_index.rb
+++ app/service/demo_service/enterprise_index.rb
@@ -1,5 +1,7 @@
 module DemoService::EnterpriseIndex
   def self.call year, province, page, amount
-    TestEnterprise.in_year(year).belong_province(province).paginate(page: page, per_page: amount)
+    enterprises = TestEnterprise.in_year(year).belong_province(province)
+    enterprises = enterprises.paginate(page: page, per_page: amount) if amount.present?
+    enterprises
   end 
 end
