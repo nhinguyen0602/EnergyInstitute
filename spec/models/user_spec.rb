88a3b290e91fb85a124d3e7735fdfcf706161aac
Nhi Nguyen <nhinguyen@novahub.vn>
2020-04-16 16:00:22 +0700

    setup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-
-  it "has a valid" do
-    expect(contact).to be_valid
+FactoryBot.define do
+  factory :user do
+    name {"test"}
+    email {"test@gmail.com"}
+    password {"1234567890"}
   end
-  
+end
+
+RSpec.describe User, type: :model do
+  describe "Validations" do
+    subject { FactoryBot.create :user }
+    
+    it "is valid with valid attributes" do
+        is_expected.to be_valid
+    end
+    
+    # it "is not valid without a title" do
+	  #   subject.title = nil
+    #     is_expected.to_not be_valid
+    # end
+    
+    # it "is not valid with too long description" do
+    #     subject.description = "a" * 501
+    #     is_expected.to_not be_valid
+    # end
+  end
 end
