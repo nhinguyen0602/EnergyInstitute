bfc78ec1bb3869bcb5e2117335c194ddaa233b3c
Nhi Nguyen <nhinguyen@novahub.vn>
2020-04-17 16:33:33 +0700

    write unit test of sign in service

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     name {"test"}
     email {"test@gmail.com"}
     password {"1234567890"}
+    password_confirmation { "1234567890" }
     association :role
   end
 end
--- spec/service/auth/sign_in_spec.rb
+++ spec/service/auth/sign_in_spec.rb
@@ -2,14 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'sign in rspec' do
   describe "sign in" do
-    subject { FactoryBot.create :user }
+    let!(:new_user) { FactoryBot.create(:user) }
+  
     let!(:user_param) do
       return {email: 'test@gmail.com', password: '1234567890' }
     end
 
     it "is success" do
       token, user = AuthService::SignIn.call(user_param)
-      expect(user).email.to eq(subject.email)
+      expect(user.email).to eq(new_user.email)
+    end
+
+    it "is false because email not found" do
+      user_param[:email] = "nhi@gmail.com"
+      expect { AuthService::SignIn.call(user_param) }.to raise_error(ExceptionHandler::AuthenticationError)
+    end
+
+    it "is false because password not correct" do
+      user_param[:password] = "00000000000"
+      expect { AuthService::SignIn.call(user_param) }.to raise_error(ExceptionHandler::AuthenticationError)
     end
 
   end
