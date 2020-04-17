2c0b56baa0c259eb3ac017da908641a08ff8d48d
Nhi Nguyen <nhinguyen@novahub.vn>
2020-04-17 09:36:07 +0700

    write test of model user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,29 +1,50 @@
 require 'rails_helper'
 
 FactoryBot.define do
+  factory :role do
+    name {"user"}
+  end
+end
+
+FactoryBot.define do
   factory :user do
     name {"test"}
     email {"test@gmail.com"}
     password {"1234567890"}
+    association :role
   end
 end
 
 RSpec.describe User, type: :model do
   describe "Validations" do
     subject { FactoryBot.create :user }
-    
     it "is valid with valid attributes" do
         is_expected.to be_valid
     end
     
-    # it "is not valid without a title" do
-	  #   subject.title = nil
-    #     is_expected.to_not be_valid
-    # end
+    it "is not valid without a name" do
+	    subject.name = nil
+        is_expected.to_not be_valid
+    end
     
-    # it "is not valid with too long description" do
-    #     subject.description = "a" * 501
-    #     is_expected.to_not be_valid
-    # end
+    it "is not valid without a email" do
+	    subject.email = nil
+        is_expected.to_not be_valid
+    end
+
+    it "is not valid without a password" do
+	    subject.password = nil
+        is_expected.to_not be_valid
+    end
+
+    it "is not valid with too long password" do
+	    subject.password = "n"*400
+        is_expected.to_not be_valid
+    end
+
+    it "does not allow duplicate email per user" do
+      user = User.new(name: 'test', email: 'test@gmail.com', password: '122234567777')
+      expect(user).to_not be_valid
+    end
   end
 end
