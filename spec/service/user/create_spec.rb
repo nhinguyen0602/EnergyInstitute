ba4df81da82085ab4fb700f027ea76ce19d33544
Nhi Nguyen <nhinguyen@novahub.vn>
2020-04-20 10:17:02 +0700

    write unit test create user service

--- spec/service/user/create_spec.rb
+++ spec/service/user/create_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe 'sign up rspec' do
-  describe "sign in" do
+RSpec.describe 'create user of admin' do
+  describe 'create user' do
     let!(:new_user) { FactoryBot.create(:user) }
   
     let!(:user_param) do
@@ -9,18 +9,18 @@ RSpec.describe 'sign up rspec' do
     end
 
     it "is success" do
-      user = AuthService::SignUp.call(user_param)
+      user = UserService::Create.call(user_param)
       expect(user.email).to eq(user_param[:email])
     end
 
     it "is false because email is existed" do
       user_param[:email] = "test@gmail.com"
-      expect { AuthService::SignUp.call(user_param) }.to raise_error(ActiveRecord::RecordInvalid)
+      expect { UserService::Create.call(user_param) }.to raise_error(ActiveRecord::RecordInvalid)
     end
 
     it "is false because email empty" do
       user_param[:name] = ""
-      expect { AuthService::SignUp.call(user_param) }.to raise_error(ActiveRecord::RecordInvalid)
+      expect { UserService::Create.call(user_param) }.to raise_error(ActiveRecord::RecordInvalid)
     end
 
   end
