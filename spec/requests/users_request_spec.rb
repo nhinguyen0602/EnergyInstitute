e523f522f2fa539eced3f60ba23b49732f693d8b
Nhi Nguyen <nhinguyen@novahub.vn>
2020-04-24 09:46:32 +0700

    test circleci

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,27 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Admin::UsersController, type: :request do
-  describe "#index" do
-    let!(:new_user) { FactoryBot.create(:user) }
-
-    let!(:user_param) do
-      return {email: 'test@gmail.com', password: '1234567890' }
-    end
-
-    before do
-      role = Role.create!(name: "admin")
-      new_user.role = role
-    end
-
-    let!(:headers) {valid_headers}
-
-    it "response successfully" do
-      byebug
-      get '/api/admin/users', headers: headers
-      expect_status 200
-    end
-
-  end
-   
 
 end
